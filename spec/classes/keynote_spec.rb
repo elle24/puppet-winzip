0981ad199c9d18f34f100f2d32d4482696416902
Administrator <administrator@TORO-iM06.local>
2015-09-23 12:40:59 +0800

    Keynote for TORO only

---
+++ spec/classes/keynote_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+#describe 'keynote' do
+#  it do
+#    should contain_package('Keynote').with({
+#      :source   => 'http://192.168.21.151/Keynote.zip',
+#      :provider => 'compressed_app'
+#    })
+#  end
+#end
